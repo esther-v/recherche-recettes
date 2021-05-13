e80686d27e0dd6bb6712779827c9b361c80c0a9a
Esther V <esvalas@gmail.com>
2021-05-13 18:17:54 +0200

    change text intro

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Search for a recipe by ingredient</h1>
+      <h1>Find the recipe you want</h1>
       <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">Search</button>
